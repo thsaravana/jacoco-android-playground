fa0612f1a12b9d330364c5b71e6514c3499446f6
thsaravana <th.saravana@gmail.com>
2021-09-01 07:36:57 +0530

    Added a ViewModel with simple methods

--- app/src/main/java/com/madrapps/playground/MainActivity.kt
+++ app/src/main/java/com/madrapps/playground/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.madrapps.playground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userId = "admin"
+        val model: MainViewModel by viewModels()
+        Log.d("Access", "Validate = ${model.validate(userId)}")
+        Log.d("Access", "Verify Access = ${model.verifyAccess(userId)}")
     }
 }
---
+++ app/src/main/java/com/madrapps/playground/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.madrapps.playground
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun validate(userId: String): Boolean {
+        return userId == "admin"
+    }
+
+    fun verifyAccess(userId: String): Boolean {
+        return userId == "super-admin"
+    }
+}
